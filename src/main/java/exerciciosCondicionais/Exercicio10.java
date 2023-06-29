e1909aadf4b3142407a640b0ecc0ebcef614e967
izabelmafisa <izabmbisneta@rd.com.br>
2023-06-29 11:56:07 -0300

    acrescimo de exercicios no repositorio

--- src/main/java/exerciciosCondicionais/Exercicio10.java
+++ src/main/java/exerciciosCondicionais/Exercicio10.java
@@ -8,23 +8,27 @@ import java.util.Scanner;
 //Exibe o menor valor lido multiplicado pelo maior e o maior valor dividido pelo menor.
 public class Exercicio10 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Digite o primeiro numero inteiro: ");
-        int num1=scanner.nextInt();
-        System.out.println("Digite o segundo numero inteiro: ");
-        int num2=scanner.nextInt();
-        System.out.println("Digite o terceiro numero inteiro: ");
-        int num3=scanner.nextInt();
-      while(num1>=0 && num2>=0 && num3 >=0)
-      {
-            int menor=Math.min(Math.min(num1,num2),num3);
-            int maior=Math.max(Math.max(num1,num2),num3);
-            int produto = menor * maior;
-            int divisao = maior / menor;
-            System.out.println("O menor valor multiplicado pelo maior: " + produto);
-            System.out.println("O maior valor dividido pelo menor: " + divisao);
-            break;
-        }
+        Scanner scanner = new Scanner(System.in);
+        int num1, num2, num3;
+        do {
+            System.out.println("Insira o primeiro valor: ");
+            num1= scanner.nextInt();
+        } while (num1<0);
+        do {
+            System.out.println("Insira o segundo valor: ");
+            num2= scanner.nextInt();
+        }while (num2<0);
+        do {
+            System.out.println("Insira o terceiro valor: ");
+            num3= scanner.nextInt();
+        }while (num3<0);
+        int menor=Math.min(Math.min(num1,num2),num3);
+        int maior=Math.max(Math.max(num1,num2),num3);
+
+        int produto=menor*maior;
+        System.out.println("O menor valor multiplicado pelo maior é: "+produto);
+        int divisao=maior/menor;
+        System.out.println("A divisão do maior valor pelo menor valor é: "+divisao);
     }
 }
 
